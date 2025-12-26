6124b8715b35fb9a5c57f2d5df39698628264193
Bukola <bukolafaduagba@gmail.com>
2025-12-26 15:21:44 +0100

    experimenting

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const home = () => {
   return (
     <div className="text-center max-w-full">
-     <p className='image-text-black text-5xl md:text-7xl font-bold leading-tight drop-shadow-sm break-words'>Candle Light <br />Studio</p>
+     <p className='image-text-black text-5xl md:text-7xl font-bold leading-tight drop-shadow-sm break-words'>Candle Light for <br />Studio</p>
     </div>
   )
 } 
