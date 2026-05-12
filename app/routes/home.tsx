9e65db8f4e5f209cbd81d7cb9d90ccb74bbf2031
Johnwickoo <bukolafaduagba@gmail.com>
2026-05-12 14:29:18 +0100

    Feat:Imported 3d camera

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import {Canvas} from '@react-three/fiber'
+import {Environment, OrbitControls, Stage} from '@react-three/drei'
+import { CameraControls, PerspectiveCamera} from '@react-three/drei'
+import { Suspense } from 'react'
+// @ts-ignore
+import { Sonya7iii } from '../components/3dModel/Sonya7iii'
+
 
 
 const home = () => {
   return (
     <div className="text-center max-w-full">
-     <p className='image-text-black text-5xl md:text-7xl font-bold leading-tight drop-shadow-sm break-words'>Candle Light for <br />Studio</p>
+     <p className='image-text-black text-5xl md:text-7xl font-bold leading-tight drop-shadow-sm break-words'>Candle Light<br />Studio</p>
+
+     <div className='border-2 h-[700px] my-5 rounded-md'>
+      <Canvas>
+        <PerspectiveCamera makeDefault fov={75} position={[0, 0, 5]} resolution={1024} />
+        <CameraControls />
+        <Suspense>
+          <Sonya7iii/>
+        </Suspense>
+       
+        <ambientLight intensity={5} />
+        <Environment preset="city" />
+      </Canvas>
+
+     </div>
     </div>
   )
 } 
